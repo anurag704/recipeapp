69acbe921ce80e83f7242e1bee74f9f127887327
anurag704 <anuragjha1093@gmail.com>
2020-09-26 20:26:19 +0530

    Updates

---
+++ precache-manifest.f2ab0815a8f205ebfdd59e63788c047c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e03841f7bea6e926d01793714ae78ab",
+    "url": "/recipeapp/index.html"
+  },
+  {
+    "revision": "0725d58e02788c2d3069",
+    "url": "/recipeapp/static/css/main.52e40a35.chunk.css"
+  },
+  {
+    "revision": "c8de07082806d16c6f6b",
+    "url": "/recipeapp/static/js/2.b73363a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipeapp/static/js/2.b73363a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0725d58e02788c2d3069",
+    "url": "/recipeapp/static/js/main.438d441c.chunk.js"
+  },
+  {
+    "revision": "2c6823dfb356579ecc04",
+    "url": "/recipeapp/static/js/runtime-main.83b6e7d9.js"
+  },
+  {
+    "revision": "431d50a80d7ac6ab7a6a51189c774c2b",
+    "url": "/recipeapp/static/media/recipe.431d50a8.webp"
+  }
+]);
